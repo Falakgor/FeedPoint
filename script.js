6ac3a2f80b7e37fb7ca3c91c6a243c81b23f2c53
Falak gor <gorfalak2@gmail.com>
2026-03-31 18:11:01 +0530

    Fix some glitches

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Login form popup handling
 document.addEventListener("DOMContentLoaded", function() {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function(e) {
-    e.preventDefault(); // form ko turant redirect hone se rokta hai
+    e.preventDefault();
 
-    // Confirm box
     let confirmLogin = confirm("Do you want to submit your login details?");
     if (confirmLogin) {
       alert("Login successful! Welcome to FeedPoint.");
